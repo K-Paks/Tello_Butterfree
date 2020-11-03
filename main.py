f0c3bd3bf8d4edd07efea9e14c068a2507210e66
K-Pax <paksyfr@gmail.com>
2020-11-03 12:57:55 +0100

    Docstring, part 1/2.

--- main.py
+++ main.py
@@ -25,11 +25,12 @@ while True:
         imgContour = img.copy()
 
         data_white = white_trackbars.get_trackbar_values()  # returns: (h_min, h_max, s_min, s_max, v_min, v_max, threshold1, threshold2, areaMin)
+        data_green = green_trackbars.get_trackbar_values()
         areaMin_white = data_white[-1]
 
         imgDil, result = prepare_img(data_white, img)
 
-        direction, area, crop_xywh = get_contours(img, imgDil, imgContour, areaMin_white, green_trackbars)
+        area = get_contours(img, imgDil, imgContour, areaMin_white, data_green)
 
         # candidate processing
         stack = stack_images(1, ([img, result], [imgDil, imgContour]))
@@ -39,16 +40,16 @@ while True:
 
         # steering
         lr, fb, ud, yaw = 0, 0, 0, 0
-        if direction == 1:
-            yaw = -40
-        elif direction == 2:
-            yaw = 40
-        elif direction == 3:
-            ud = 40
-        elif direction == 4:
-            ud = -40
-        else:
-            ud, yaw = 0, 0
+        # if direction == 1:
+        #     yaw = -40
+        # elif direction == 2:
+        #     yaw = 40
+        # elif direction == 3:
+        #     ud = 40
+        # elif direction == 4:
+        #     ud = -40
+        # else:
+        #     ud, yaw = 0, 0
 
         if area < 4000 and area != 0:
             fb = 15
@@ -57,16 +58,7 @@ while True:
         else:
             fb = 0
 
-        # print(fb)
-
         drone.rc_control(lr, fb, ud, yaw)
-        # print(drone.get_acceleration())
-
-        # cv2.imshow('Cap', img)
-        # cv2.imshow('Cap2', imgHsv)
-
-
-
 
 
         # Video Stream is closed if escape key is pressed
--- processing.py
+++ processing.py
@@ -1,12 +1,15 @@
 import cv2.cv2 as cv2
 import numpy as np
+from utils import *
 
-# test
+
 def empty(a):
     pass
 
+
 # class used to create trackbar window and get their parameters
 class TrackbarWindow:
+    """Class used to create trackbar window and store it's values."""
     def __init__(self, num, color=None):
         if color == 'white':  # predefined values for mapping whites only
             init_h_min, init_h_max, init_s_min, init_s_max, init_v_min, init_v_max = 0, 179, 0, 59, 220, 255
@@ -35,6 +38,7 @@ class TrackbarWindow:
         self.num = num
 
     def get_trackbar_values(self):
+        """Method used to getting the trackbars' values."""
         h_min = cv2.getTrackbarPos(f"HUE Min_{self.num}", f"HSV_{self.num}")
         h_max = cv2.getTrackbarPos(f"HUE Max_{self.num}", f"HSV_{self.num}")
         s_min = cv2.getTrackbarPos(f"SAT Min_{self.num}", f"HSV_{self.num}")
@@ -49,29 +53,74 @@ class TrackbarWindow:
         return h_min, h_max, s_min, s_max, v_min, v_max, threshold1, threshold2, area_min
 
 
-def get_candidate_contours(img, img_contour, area_min):
+def get_candidate_contours(img, candidate_w_contours, area_min):
+    """If sufficient candidate is found, returns `1` and draws contours and boundingRect around it.
+
+    Parameters:
+    img (np.ndarray): Image preprocessed by `prepare_img` function.
+    candidate_w_contours (np.ndarray): Copy of the original image, on which the contours are going to be applied
+    area_min (int): Minimal area of an object to become a candidate
+
+    Returns:
+    int: 1 if candidate found, else 0.
+    """
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     for cnt in contours:
         area = cv2.contourArea(cnt)
         if area > area_min:
-            cv2.drawContours(img_contour, cnt, -1, (255, 0, 255), 7)
+            cv2.drawContours(candidate_w_contours, cnt, -1, (255, 0, 255), 7)
             peri = cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
-
             x, y, w, h = cv2.boundingRect(approx)
-            # crop_xywh = x, y, w, h
 
-            cv2.rectangle(img_contour, (x, y), (x + w, y + h), (0, 255, 0), 5)
+            cv2.rectangle(candidate_w_contours, (x, y), (x + w, y + h), (0, 255, 0), 5)
             return 1
     return 0
 
 
-def get_contours(raw_img, img, img_contour, area_min, green_trackbars=None):
-    direction = 0
-    area = 0
-    crop_xywh = None
+def create_candidates(crop_xywh, raw_img, cnt):
+    """Function that, if a object is found, returns a candidate image of it.
 
+    Parameters:
+    crop_xywh (np.ndarray): an array of bounding box values [x,  y, width, height].
+    raw_img (np.ndarray): original drone image.
+    cnt (np.ndarray): contours for a given object.
+
+    Returns:
+    candidate_img (np.ndarray): the original image cropped into the found object bounding box's size.
+    candidate_cropped (np.ndarray): a cropped version of the candidate image.
+    """
+    if crop_xywh is not None:
+        x, y, w, h = crop_xywh
+        candidate_img = raw_img[y:y + h, x:x + w]
+        candidate_img = cv2.resize(candidate_img, (320, 240))
+
+        mask = np.ones(raw_img.shape[:2])
+        mask = cv2.drawContours(mask, [cnt], -1, 0, cv2.FILLED)
+        candidate_cropped = raw_img.copy()
+        candidate_cropped[mask.astype(np.bool), :] = 0
+
+        candidate_cropped = candidate_cropped[y:y + h, x:x + w]
+        candidate_cropped = cv2.resize(candidate_cropped, (320, 240))
+    else:
+        candidate_img = np.zeros((240, 320, 3), np.uint8)
+        candidate_cropped = np.zeros((240, 320, 3), np.uint8)
+    return candidate_img, candidate_cropped
+
+
+def get_contours(img, img_contour, area_min, raw_img, data_green):
+    """Function that finds the objects' contours, finds the candidate object and invokes functions
+    that crop it into stand-alone image.
+
+    Parameters:
+    img (np.ndarray): Image preprocessed by `prepare_img` function.
+    img_contour (np.ndarray): #TODO
+    area_min (int): Minimal area of an object to become recognized.
+    raw_img (np.ndarray): the original image from the drone.
+    data_green (#TODO): trackbars' parameters for the green color recognition.
+    """
+    area = 0  # if contours not found, area = 0
     frame_h, frame_w = list(img.shape)
 
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
@@ -83,43 +132,29 @@ def get_contours(raw_img, img, img_contour, area_min, green_trackbars=None):
             cv2.drawContours(img_contour, cnt, -1, (255, 0, 255), 3)
             peri = cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+            xywh = cv2.boundingRect(approx)
 
-            x, y, w, h = cv2.boundingRect(approx)
-            crop_xywh = x, y, w, h
+            candidate_img, candidate_cropped = create_candidates(xywh, raw_img, cnt)
+            candidate_for_contours = candidate_img.copy()
 
-            if crop_xywh is not None:
-                x, y, w, h = crop_xywh
-                candidate_img = raw_img[y:y + h, x:x + w]
-                candidate_img = cv2.resize(candidate_img, (320, 240))
-
-                mask = np.ones(raw_img.shape[:2])
-                mask = cv2.drawContours(mask, [cnt], -1, 0, cv2.FILLED)
-                candidate_img2 = raw_img.copy()
-                candidate_img2[mask.astype(np.bool), :] = 0
-
-                candidate_img2 = candidate_img2[y:y + h, x:x + w]
-                candidate_img2 = cv2.resize(candidate_img2, (320, 240))
-            else:
-                candidate_img = np.zeros((240, 320, 3), np.uint8)
-                candidate_img2 = np.zeros((240, 320, 3), np.uint8)
-
-            cand_contour = candidate_img.copy()
-            data_green = green_trackbars.get_trackbar_values()
             area_min_green = data_green[-1]
-
             cand_dil, cand_res = prepare_img(data_green, candidate_img)
-            correct_candidate = get_candidate_contours(cand_dil, cand_contour, area_min_green)
+            correct_candidate = get_candidate_contours(cand_dil, candidate_for_contours, area_min_green)
 
             if not correct_candidate:
                 no_candidate = np.zeros((240, 320, 3), np.uint8)
-                candidate_img2 = np.zeros((240, 320, 3), np.uint8)
-                stack_cand = stack_images(1, ([no_candidate, cand_res, img_contour], [cand_dil, candidate_img2, np.zeros((240, 320, 3), np.uint8)]))
+                candidate_cropped = np.zeros((240, 320, 3), np.uint8)
+                stack_cand = stack_images(1, ([no_candidate, cand_res, img_contour], [cand_dil, candidate_cropped, np.zeros((240, 320, 3), np.uint8)]))
                 cv2.imshow('candidate', stack_cand)
 
+                area = 0
+                return area
+
             elif correct_candidate:
+                x, y, w, h = xywh
                 cv2.rectangle(img_contour, (x, y), (x + w, y + h), (0, 255, 0), 5)
 
-                stack_cand = stack_images(1, ([candidate_img, cand_res, img_contour], [cand_dil, candidate_img2, np.zeros((240, 320, 3), np.uint8)]))
+                stack_cand = stack_images(1, ([candidate_img, cand_res, img_contour], [cand_dil, candidate_cropped, np.zeros((240, 320, 3), np.uint8)]))
                 cv2.imshow('candidate', stack_cand)
 
                 cx = int(x + (w / 2))
@@ -129,6 +164,6 @@ def get_contours(raw_img, img, img_contour, area_min, green_trackbars=None):
                 cv2.line(img_contour, (int(frame_w / 2), int(frame_h / 2)), (cx, cy),
                          (0, 0, 255), 3)
 
-                return direction, area, crop_xywh
-    return direction, area, crop_xywh
+                return area  # if correct candidate found, return the area
+    return area
 
